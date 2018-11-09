b5f44037b4953cccbe23cc6414387f374c0cf36a
jhatts <jhatts@hotmail.co.uk>
2018-11-09 21:44:12 +0000

    added javascript to connect to API

--- app.js
+++ app.js
@@ -1,2 +1,25 @@
-const quoteButton = document.querySelector('.quote-button');
-const tweetButton = document.querySelector('.tweet-button');
+const quoteButton = document.getElementById('quote-button');
+const tweetButton = document.getElementById('tweet-button');
+
+quoteButton.addEventListener('click', getQuote);
+
+const endpoint = 'http://quotes.rest/qod';
+
+function getQuote() {
+    // create a request variable and assign to new XMLHttpRequest to it
+    var request = new XMLHttpRequest();
+
+    // Open a new connection using the GET request
+    request.open('GET', endpoint, true);
+
+    request.onload = function() {
+        // Access JSON data here
+        if(this.status == 200) {
+            var quote = JSON.parse(this.responseText);
+            console.log(quote);
+        }
+    }
+    // Send Request
+    request.send();
+}
+
