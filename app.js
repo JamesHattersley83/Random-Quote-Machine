1c234134ca6b855bb0792fc2965eef5482950755
jhatts <jhatts@hotmail.co.uk>
2018-11-14 00:18:50 +0000

    added share quote to twitter functionality

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
-const quoteButton = document.getElementById('quote-button');
+const quoteButton = document.querySelector('#quote-button');
 const tweetButton = document.querySelector('.tweet-button');
 
 quoteButton.addEventListener('click', getQuote);
-tweetButton.addEventListener('click', tweetQuote);
 
 const endpoint = 'https://talaikis.com/api/quotes/random/';
 
@@ -17,6 +16,7 @@ function getQuote() {
         // Access JSON data here
         if(this.status == 200) {
             const quoteText = JSON.parse(this.response);
+
             // display quote text
             const displayQuote = document.querySelector('.item');
             displayQuote.textContent = quoteText.quote;
@@ -24,6 +24,11 @@ function getQuote() {
             // display quote author
             const displayAuthor = document.querySelector('.author');
             displayAuthor.textContent = quoteText.author;
+
+            // Tweet quote
+            tweetButton.addEventListener('click', function() {
+                tweetButton.setAttribute('href', 'https://twitter.com/intent/tweet/?text=' + quoteText.quote + '- ' + quoteText.author);
+            });
         } else {
             console.log('An error has occurred');
         }
@@ -32,6 +37,3 @@ function getQuote() {
     request.send();
 }
 
-function tweetQuote() {
-    tweetButton.setAttribute('href', `https://twitter.com/share`);
-}
